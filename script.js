c7e0ef76f5c96e0d240ca06fb696073f38f70043
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-12 16:05:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<a href="https://www.amazon.in/stores/DELUZEX/page/C2CF2F7D-46ED-433F-B75B-0A39923D937A?lp_asin=B0GDMKP1PD&ref_=ast_bln&store_ref=bl_ast_dp_brandlogo_sto">Shop Now</a>
+
+<a href="https://www.amazon.in/stores/DELUZEX/page/C2CF2F7D-46ED-433F-B75B-0A39923D937A?lp_asin=B0GDMKP1PD&ref_=ast_bln&store_ref=bl_ast_dp_brandlogo_sto">
+    <button class="shop-btn">Shop Now</button>
+</a>
